3a08dce7e441dede60683b5f9d3792bd144e351d
Oliver Öhrström <xzereha@live.se>
2025-10-06 13:38:14 +0200

    Rewrote Product to more ergonomic usage

--- src/main/java/com/example/Product.java
+++ src/main/java/com/example/Product.java
@@ -1,19 +1,37 @@
 package com.example;
 
 public class Product {
-    private final String id;
+    /** Auto incrementing ID */
+    private static long nextID = 0;
+    private final long id;
     private String name;
     private String category;
     private double price;
 
-    public Product(String id, String name, String category, double price) {
-        this.id = id;
+    /**
+     * Create a new product
+     * 
+     * @param name     The name of the product, must not be empty.
+     * @param category The category the product belongs to, must not be empty.
+     * @param price    The price of the product, this mut not be a negative number.
+     * @throws IllegalArgumentException if any of the parameters is null or an
+     *                                  invalid value.
+     */
+    public Product(String name, String category, double price) {
+        if (name == null || name.isEmpty())
+            throw new IllegalArgumentException("Name mut be a valid non-empty string");
+        if (category == null || category.isEmpty())
+            throw new IllegalArgumentException("Category must be a valid non-empty string");
+        if (price <= 0.0)
+            throw new IllegalArgumentException("Price must not be negative");
+
+        this.id = nextID++;
         this.name = name;
         this.category = category;
         this.price = price;
     }
 
-    public String getId() {
+    public long getId() {
         return id;
     }
 
@@ -24,13 +42,12 @@ public class Product {
     /**
      * Change the name of the product.
      * 
-     * @param name The new name of the product, this must not be empty.
+     * @param name The new name of the product, if the name is empty this call is
+     *             ignored.
      */
     public void setName(String name) {
         if (!name.isEmpty())
             this.name = name;
-        else
-            System.out.println("setName requires a non-empty string");
     }
 
     public String getCategory() {
@@ -40,23 +57,25 @@ public class Product {
     /**
      * Change the category of the product.
      * 
-     * @param category The new category of the product, this must not be empty.
+     * @param category The new category of the product, if the category is empty
+     *                 this call is ignored.
      */
     public void setCategory(String category) {
         if (!category.isEmpty())
             this.category = category;
-        else
-            System.out.println("setCategory requires a non-empty string");
     }
 
     public double getPrice() {
         return price;
     }
 
+    /**
+     * Updates the price of the Product
+     * 
+     * @param price New price, will be ignored if set to a negative value.
+     */
     public void setPrice(double price) {
         if (price >= 0.0)
             this.price = price;
-        else
-            System.out.println("setPrice can't have a negative price");
     }
 }
